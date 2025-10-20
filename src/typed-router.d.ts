7270bdbe02325d4ec68507328c1147caff109c28
Doji <138183055+BeanDz@users.noreply.github.com>
2025-10-21 00:50:02 +0800

    create basic vault UI (#11)

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/create-vault': RouteRecordInfo<'/create-vault', '/create-vault', Record<never, never>, Record<never, never>>,
   }
 
   /**
@@ -34,6 +35,10 @@ declare module 'vue-router/auto-routes' {
   export interface _RouteFileInfoMap {
     'src/pages/index.vue': {
       routes: '/'
+      views: never
+    }
+    'src/pages/create-vault.vue': {
+      routes: '/create-vault'
       views: never
     }
   }
