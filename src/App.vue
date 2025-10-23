293c636a832fd0f7bfeabc6c90c7a8fe1a5180d4
Doji <138183055+BeanDz@users.noreply.github.com>
2025-10-23 19:37:06 +0800

    feat: login barrier (#17)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { useSessionsStore } from '~/stores/sessions'
+
+const router = useRouter()
+
+router.beforeEach(async (to) => {
+  const sessionsStore = useSessionsStore()
+  if (!sessionsStore.session)
+    await sessionsStore.getSession()
+  const loggingIn = !!sessionsStore.session
+  const loggingInRoutes = ['/create-vault', '/my-vault', '/withdraw']
+  if (loggingInRoutes.includes(to.path) && !loggingIn)
+    return '/'
+})
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import type { UserModule } from './types'
-
 import { setupLayouts } from 'layouts-generated'
 import { createApp, vaporInteropPlugin } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
-
 import '@unocss/reset/tailwind.css'
-
 import 'vue-sonner/style.css'
-
 import './styles/main.css'
 import 'uno.css'
 
